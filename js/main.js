847fa9e3369dc8647d91793b5218e8493978f545
Motaz Ramadan <120125123+motazramadan8@users.noreply.github.com>
2023-05-24 19:57:01 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,1 @@
+let icon=document.querySelector(".menu_icon"),links=document.getElementById("smallLinks");function res_nav(){links.classList.toggle("small-links")}icon.addEventListener("click",()=>{icon.classList.toggle("clicked")});
