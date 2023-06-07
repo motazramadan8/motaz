50076a4ca51b5c6bb60cf795efb58e04a423b513
Motaz Ramadan <120125123+motazramadan8@users.noreply.github.com>
2023-06-07 14:41:38 +0300

    Delete js directory

--- js/main.js
+++
@@ -1,1 +0,0 @@
-let icon=document.querySelector(".menu_icon"),links=document.getElementById("smallLinks");function res_nav(){links.classList.toggle("small-links")}icon.addEventListener("click",()=>{icon.classList.toggle("clicked")});
